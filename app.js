e31c4cc4ff60b0a07b17371fb1139691e6b4c886
AsimEqubal <asimequbal.web@gmail.com>
2023-11-27 11:05:32 +0530

    arry

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-const arr = [25,96,16,65,45,36];
-arr.map(Math.sqrt) = document.getElementById('demo').innerHTML;
+const car=['maruti suzuki','volvo','tata','newcar'];
+let text = '';
+for(let i = 0; i < car.length; i++){
+    text += car[i] + '<br>'
+};
+document.getElementById('demo').innerHTML = text;
+// const cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
+
+// let text = "";
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = text;
